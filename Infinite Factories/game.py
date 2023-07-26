7427c2ef751abeb96c0a9503941bfbf291238156
Finn2763 <100381584+Finn2763@users.noreply.github.com>
2023-07-26 09:14:35 -0400

    Update game.py

--- Infinite Factories/game.py
+++ Infinite Factories/game.py
@@ -9,7 +9,7 @@ pygame.init()
 
 displayInfo = pygame.display.Info()
 
-def change_window_size(existing_surface, new_width, new_height): # Code by ChatGPT
+def change_window_size(existing_surface, new_width, new_height):
     # Create a new display surface with the desired dimensions
     new_surface = pygame.display.set_mode((new_width, new_height), pygame.SCALED)
     
@@ -39,13 +39,23 @@ generation = 0
 shop1 = 0
 shop2 = 0
 shop3 = 0
+shop4 = 0
+shop5 = 0
+shop6 = 0
+shop7 = 0
+shop8 = 0
 smoothfps = 60
 lasttime = round(time.time()/10)
 newss = 0
-news = ["237 Lines of Code!", "hi :)", "asdfghjkl!!!", "why are you here", "fard", 'what are you even making? what even is a "point"?', "eh", "i don't know", "ZeroDivisionError: division by zero", '"siiiaaaaaaiifbbbbbbbbbbbbbfgs[]]]]]]]]]];;;;;;;;;;zzzzzzzqqqqqqqqqqq" -my cat', "11th news message!", "Breaking news: Nobody still knows what a point is supposed to be.", "😎 nooo my emoji isn't working :(", "the next peice of news is a lie!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", "{} is a big number".format(points),"You matter! (unless you multiply yourself by c^2, then you energy)", "This sentence is false.", "{} total news messages".format(newss)]
+news = ["365 Lines of Code!", "hi :)", "asdfghjkl!!!", "why are you here", "fard", 'what are you even making? what even is a "point"?', "eh", "i don't know", "ZeroDivisionError: division by zero", '"siiiaaaaaaiifbbbbbbbbbbbbbfgs[]]]]]]]]]];;;;;;;;;;zzzzzzzqqqqqqqqqqq" -my cat', "11th news message!", "Breaking news: Nobody still knows what a point is supposed to be.", "😎 nooo my emoji isn't working :(", "the next peice of news is a lie!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", "{} is a big number".format(points),"You matter! (unless you multiply yourself by c^2, then you energy)", "This sentence is false.", "{} total news messages".format(newss)]
 currentNews = random.choice(news)
 shop2show = False
 shop3show = False
+shop4show = False
+shop5show = False
+shop6show = False
+shop7show = False
+shop8show = False
 lastf11 = False
 fullscreen = False
 
@@ -74,6 +84,11 @@ def notate(number):
 shopitem1 = pygame.Rect(100, 200, 500, 100)
 shopitem2 = pygame.Rect(100, 350, 500, 100)
 shopitem3 = pygame.Rect(100, 500, 500, 100)
+shopitem4 = pygame.Rect(100, 650, 500, 100)
+shopitem5 = pygame.Rect(600, 200, 500, 100)
+shopitem6 = pygame.Rect(600, 350, 500, 100)
+shopitem7 = pygame.Rect(600, 500, 500, 100)
+shopitem8 = pygame.Rect(600, 650, 500, 100)
 
 print("Loading Save...")
 
@@ -89,7 +104,6 @@ def read(line): #Code by GPT-3.5
     with open(file_path, 'r') as file:
         lines = file.readlines()
         if 1 <= line <= len(lines):
-            print(lines[line - 1])
             return lines[line - 1]
         else:
             return 0
@@ -106,15 +120,17 @@ def write(line_number, new_content):
         lines[line_number - 1] = str(new_content) + '\n'
         with open(file_path, 'w') as file:
             file.writelines(lines)
-        print(f"Successfully wrote to line {line_number}.")
-    else:
-        print(f"Line {line_number} does not exist in the file.")
     file.close
 
 points = float(read(1))
 shop1 = int(read(2))
 shop2 = int(read(3))
 shop3 = int(read(4))
+shop4 = int(read(5))
+shop5 = int(read(6))
+shop6 = int(read(7))
+shop7 = int(read(8))
+shop8 = int(read(9))
 
 print ("Have Fun!")
 
@@ -143,6 +159,26 @@ while running: # Game Loop
                 if points >= 150000:
                     points -= 150000
                     shop3+=1
+            elif mouseinside(shopitem4):
+                if points >= 1000000:
+                    points -= 1000000
+                    shop4+=1
+            elif mouseinside(shopitem5):
+                if points >= 15000000:
+                    points -= 15000000
+                    shop5+=1
+            elif mouseinside(shopitem6):
+                if points >= 50000000:
+                    points -= 50000000
+                    shop6+=1
+            elif mouseinside(shopitem7):
+                if points >= 200000000:
+                    points -= 200000000
+                    shop7+=1
+            elif mouseinside(shopitem8):
+                if points >= 1000000000:
+                    points -= 1000000000
+                    shop8+=1
             lastmouse = True
     else:
         lastmouse = False
@@ -179,6 +215,66 @@ while running: # Game Loop
         text("shop3Amount", "x" + str(shop3), 280, 560, 0)
         text("shop3Price", "150K Points", 120, 560, 0)
     
+    if shop4show:
+        if not mouseinside(shopitem4):
+            pygame.draw.rect(window, (0, 55, 110), shopitem4) 
+        else:
+            pygame.draw.rect(window, (0, 27, 55), shopitem4)
+
+        pygame.draw.rect(window, (114, 147, 168), shopitem4, width=10)
+
+        text("shop4Text", "Tier IV Factory", 120, 655, 1)
+        text("shop4Amount", "x" + str(shop4), 260, 710, 0)
+        text("shop4Price", "1M Points", 120, 710, 0)
+
+    if shop5show:
+        if not mouseinside(shopitem5):
+            pygame.draw.rect(window, (0, 55, 110), shopitem5) 
+        else:
+            pygame.draw.rect(window, (0, 27, 55), shopitem5)
+
+        pygame.draw.rect(window, (114, 147, 168), shopitem5, width=10)
+
+        text("shop1Text", "Tier V Factory", 620, 205, 1)
+        text("shop1Amount", "x" + str(shop5), 745, 260, 0)
+        text("shop1Price", "15M Points", 620, 260, 0)
+
+    if shop6show:
+        if not mouseinside(shopitem6):
+            pygame.draw.rect(window, (0, 55, 110), shopitem6) 
+        else:
+            pygame.draw.rect(window, (0, 27, 55), shopitem6)
+
+        pygame.draw.rect(window, (114, 147, 168), shopitem6, width=10)
+
+        text("shop2Text", "Tier VI Factory", 620, 355, 1)
+        text("shop2Amount", "x" + str(shop6), 760, 410, 0)
+        text("shop2Price", "50M Points", 620, 410, 0)
+    
+    if shop7show:
+        if not mouseinside(shopitem7):
+            pygame.draw.rect(window, (0, 55, 110), shopitem7) 
+        else:
+            pygame.draw.rect(window, (0, 27, 55), shopitem7)
+
+        pygame.draw.rect(window, (114, 147, 168), shopitem7, width=10)
+
+        text("shop3Text", "Tier VII Factory", 620, 505, 1)
+        text("shop3Amount", "x" + str(shop7), 780, 560, 0)
+        text("shop3Price", "200M Points", 620, 560, 0)
+
+    if shop8show:
+        if not mouseinside(shopitem8):
+            pygame.draw.rect(window, (0, 55, 110), shopitem8) 
+        else:
+            pygame.draw.rect(window, (0, 27, 55), shopitem8)
+
+        pygame.draw.rect(window, (114, 147, 168), shopitem8, width=10)
+
+        text("shop4Text", "Tier VIII Factory", 620, 655, 1)
+        text("shop4Amount", "x" + str(shop8), 760, 710, 0)
+        text("shop4Price", "1B Points", 620, 710, 0)
+
     pygame.draw.rect(window, (114, 147, 168), shopitem1, width=10)
 
     if lasttime != round(time.time()/10):
@@ -190,14 +286,40 @@ while running: # Game Loop
         write(2, shop1)
         write(3, shop2)
         write(4, shop3)
+        write(5, shop4)
+        write(6, shop5)
+        write(7, shop6)
+        write(8, shop7)
+        write(9, shop8)
+        print("Game saved with {} points.".format(notate(points)))
 
     lasttime = round(time.time()/10)
 
-    if points >= 5000:
+    if pygame.key.get_focused():
+        pygame.mixer.music.set_volume(0.3)
+    else:
+        pygame.mixer.music.set_volume(0)
+
+    if points >= 1000:
         shop2show = True
     
-    if points >= 30000:
+    if points >= 15000:
         shop3show = True
+
+    if points >= 100000:
+        shop4show = True
+
+    if points >= 1500000:
+        shop5show = True
+    
+    if points >= 5000000:
+        shop6show = True
+
+    if points >= 20000000:
+        shop7show = True
+
+    if points >= 100000000:
+        shop8show = True
 
     text("pointsText", "Points: " + str(notate(points)), 100, 100, 1)
     text("speedText", "Generation: " + str(notate(generation)), 100, 150, 0)
@@ -210,7 +332,7 @@ while running: # Game Loop
     text("newsText", currentNews, 25, 50, 0)
     text("creditText", "Music by Cursedsnake", 25, pygame.display.get_window_size()[1]-50, 0)
 
-    generation = (shop1 + (shop2*20) + (shop3*400))
+    generation = (shop1 + (shop2*20) + (shop3*400) + (shop4*8000))
 
     newss = len(news)
     news[17] = "{} total news messages".format(newss)
@@ -224,6 +346,12 @@ while running: # Game Loop
             write(2, shop1)
             write(3, shop2)
             write(4, shop3)
+            write(5, shop4)
+            write(6, shop5)
+            write(7, shop6)
+            write(8, shop7)
+            write(9, shop8)
+            print("Game saved with {} points.".format(notate(points)))
             running = False
 
         if event.type == pygame.KEYDOWN:
